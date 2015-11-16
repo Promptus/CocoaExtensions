07ebfb8876db3cd33ff031fc32efee0898c1d823
Lars Kuhnt <lars.kuhnt@gmail.com>
2015-11-16 14:49:30 +0100

    initialize main object context with NSMainQueueConcurrencyType

--- CocoaExtensions/Managers/CoreDataManager.m
+++ CocoaExtensions/Managers/CoreDataManager.m
@@ -57,7 +57,7 @@
   
   NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
   if (coordinator != nil) {
-    managedObjectContext = [[NSManagedObjectContext alloc] init];
+    managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
     managedObjectContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
     [managedObjectContext setPersistentStoreCoordinator:coordinator];
   }
